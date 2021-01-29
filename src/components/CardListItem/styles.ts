c68cd80841ffecb29428edeb9779bc17c152cecd
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2021-01-29 19:46:38 -0300

    Improve CardListItem

--- src/components/CardListItem/styles.ts
+++ src/components/CardListItem/styles.ts
@@ -1,48 +1,56 @@
-import styled from 'styled-components/native'
-import { AntDesign } from '@expo/vector-icons'
+import styled from "styled-components/native";
+import { AntDesign } from "@expo/vector-icons";
 
-import { Colors, Spacing } from '../../styles'
+import { Colors, Spacing } from "../../styles";
 
-export const Card = styled.View`
-  backgroundColor: ${Colors.cardColor};
-  marginHorizontal: ${Spacing.padding}px;
-  shadowOffset: {
+export const Card = styled.TouchableOpacity`
+  background-color: ${Colors.cardColor};
+  margin-horizontal: ${Spacing.padding}px;
+  shadow-offset: {
     width: 0;
     height: 5;
-  };
-  shadowOpacity: 0.25;
-  shadowRadius: 20px;
+  }
+  shadow-opacity: 0.25;
+  shadow-radius: 20px;
   elevation: 2;
-`
+`;
 
 export const Details = styled.View`
   padding: ${Spacing.padding}px;
-`
+`;
 
 export const Header = styled.View`
-  flexDirection: row;
-  justifyContent: space-between;
-  paddingVertical: ${Spacing.extraMargin}px;
-`
+  flex-direction: row;
+  justify-content: space-between;
+  padding-vertical: ${Spacing.extraMargin}px;
+`;
 
 export const Model = styled.Text`
   color: ${Colors.textColor};
-  fontSize: 30px;
-  fontFamily: Arial;
-`
+  font-size: 30px;
+  font-family: Arial;
+`;
 
 export const Line = styled.View`
   height: 1px;
-  backgroundColor: ${Colors.divisorColor};
-  marginVertical: ${Spacing.extraMargin}px;
-`
+  background-color: ${Colors.divisorColor};
+  margin-vertical: ${Spacing.extraMargin}px;
+`;
 
 export const MakeYear = styled.Text`
-  paddingVertical: ${Spacing.extraMargin}px;
-`
+  padding-vertical: ${Spacing.extraMargin}px;
+`;
 
-export const StarIcon = styled(AntDesign).attrs(props => ({
-  name: props.star ? 'star' : 'staro',
-  color: props.star ? Colors.starColor : Colors.textColor,
-  size: 24
-}))``
+interface IStarIcon {
+  star: boolean;
+}
+
+export const StarIcon = styled(AntDesign).attrs((props: IStarIcon) => {
+  console.log({ props });
+
+  return {
+    name: props.star ? "star" : "staro",
+    color: props.star ? Colors.starColor : Colors.textColor,
+    size: 24,
+  };
+})``;
